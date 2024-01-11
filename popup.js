431f934355e70639e3876bf5f9c6886c682976be
EdithMenechi <edith.menechi@gmail.com>
2024-01-11 11:00:11 +0100

    traduction ok

--- popup.js
+++ popup.js
@@ -4,7 +4,30 @@
 
   console.log(response);
 
-  document.getElementById('trad').innerHTML = response;
+//traduire response avant de l'envoyer
+
+
+const translationResponse = await fetch('https://api-free.deepl.com/v2/translate', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'DeepL-Auth-Key votreClefAPI:fx',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      text: [response],
+      target_lang: 'FR'
+    })
+  });
+
+
+  const translationData = await translationResponse.json();
+  const translatedResponse = translationData.translations[0].text;
+  const language = translationData.translations[0].detected_source_language;
+
+  console.log(translationData)
+
+  document.getElementById('trad').innerHTML = `Langue détectée : ${language} Traduction : ${translatedResponse}`;
 })();
+
 
 
