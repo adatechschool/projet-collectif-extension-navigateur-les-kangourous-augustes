8eb313e5c37d0c819b157db54e3a7ce84f99fb39
Jean-Clément <jeanclement@audusseau.fr>
2024-01-11 16:50:22 +0100

    ajout de la feature de cliquettement du kangourou

--- popup.js
+++ popup.js
@@ -41,12 +41,11 @@ function changeLangue(lettre) {
 (async () => {
   const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
   const response = await chrome.tabs.sendMessage(tab.id, { action: "declenche" });
-
+  
   console.log(response);
+  //traduire response avant de l'envoyer
 
-//traduire response avant de l'envoyer
-
-
+  
 const translationResponse = await fetch('https://api-free.deepl.com/v2/translate', {
     method: 'POST',
     headers: {
@@ -70,3 +69,6 @@ const translationResponse = await fetch('https://api-free.deepl.com/v2/translate
   document.getElementById('lang').innerHTML = `Langage : ${language}`;
   document.getElementById('trad').innerHTML = `Traduction : ${translatedResponse}`;
 })();
+
+let son = new Audio("cliquetis.flac")
+son.play()
