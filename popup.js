f3e7f1b30d808ddda6a25b90311a4f0a2013f0da
EdithMenechi <edith.menechi@gmail.com>
2024-01-11 11:00:11 +0100

    traduction ok

--- popup.js
+++ popup.js
@@ -1,10 +1,38 @@
 (async () => {
-  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-  const response = await chrome.tabs.sendMessage(tab.id, { action: "declenche" });
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  const response = await chrome.tabs.sendMessage(tab.id, {
+    action: "declenche",
+  });
 
   console.log(response);
 
-  document.getElementById('trad').innerHTML = response;
+  //traduire response avant de l'envoyer
+
+  const translationResponse = await fetch(
+    "https://api-free.deepl.com/v2/translate",
+    {
+      method: "POST",
+      headers: {
+        Authorization: "DeepL-Auth-Key votreClefApi:fx",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: [response],
+        target_lang: "FR",
+      }),
+    }
+  );
+
+  const translationData = await translationResponse.json();
+  const translatedResponse = translationData.translations[0].text;
+  const language = translationData.translations[0].detected_source_language;
+
+  console.log(translationData);
+
+  document.getElementById(
+    "trad"
+  ).innerHTML = `Langue détectée : ${language} Traduction : ${translatedResponse}`;
 })();
-
-
