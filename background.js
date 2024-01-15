2110b44d0a4143f6d2c566235d633493ed72314b
EdithMenechi <edith.menechi@gmail.com>
2024-01-15 17:49:01 +0100

    rien ne fonctionne pour le choix de la langue, mais fin de journée

--- background.js
+++ background.js
@@ -1,15 +1,16 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+function stockerLangue(langue) {
+    chrome.storage.local.set({ langue: langue }, function() {
+      console.log("Langue de traduction stockée avec succès : " + langue);
+    });
+  }
+  
+  // Exemple d'utilisation de la fonction pour stocker la langue
+  stockerLangue("fr"); // Remplacez "fr" par la langue de votre choix
 
-    if (localStorage.getItem("langue") == null) {
-        const langue = "FR";
-    } else {
-        let langue = localStorage.getItem("langue")
-    }
-
-chrome.tabs.executeScript({
-    
-
-
-});
-
-});
+chrome.storage.local.get("langues", function(result) {
+    var selectedLanguage = result.langues;
+    console.log("Langue de traduction récupérée : " + selectedLanguage);
+  
+    // Utiliser la langue de traduction dans votre appel à l'API de traduction
+    // ...
+  });
