b5ad3781a918c885174abcd2e8f55eb772e5a8be
EdithMenechi <edith.menechi@gmail.com>
2024-01-17 16:01:58 +0100

    résolution d'un conflit de merge + ménage dans les fichiers

--- popup.js
+++ popup.js
@@ -89,11 +89,7 @@ const translationResponse = await fetch('https://api-free.deepl.com/v2/translate
 
     let language = changeLangue(codeLanguage)
     //Transformation de la langue de la sélection
-<<<<<<< Updated upstream
-    document.getElementById('lang').innerHTML = `Langue : ${language}`;
-=======
-    document.getElementById('lang').innerHTML = `<span class = "bold-text">Langage :</span> ${language}`;
->>>>>>> Stashed changes
+    document.getElementById('lang').innerHTML = `<span class = "bold-text">Langue :</span> ${language}`;
     //Affichage de la langue de la sélection
     document.getElementById('trad').innerHTML = `<span class = "bold-text">Traduction :</span> ${translatedResponse}`;
     //Affichage de la traduction
