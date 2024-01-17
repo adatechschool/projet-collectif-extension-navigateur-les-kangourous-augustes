f091b3477e02d7ee8412e1b294d62501cfac0d36
Jean-Clément <jeanclement@audusseau.fr>
2024-01-17 14:28:10 +0100

    proposition de code pour activer/désactiver le son de l’extension

--- options.js
+++ options.js
@@ -7,7 +7,7 @@ const saveOptions = () => {
       //Enregistre l'info dans chrome.storage qu'on a choisi en sync pour harmoniser 
       //nos différents appareils
       //.set : envoyer
-      { langueDeTraduction: langue },
+      { langueDeTraduction: langue }, //nomDeLaClé: nom de la valeur
       () => {
         // Met à jour 'status' dans html pour que l'utilisateur sache que l'option est enregistrée
         const status = document.getElementById('status');
@@ -16,9 +16,24 @@ const saveOptions = () => {
       }
     );
   };
+
+  const saveAudio = () => {
+    const configAudio = document.getElementById('toggleAudio').checked;
+    console.log(configAudio)
+    console.log("coucou audio")
+    chrome.storage.sync.set(
+      { choixAudio: configAudio }
+    )
+  }
   
   // Réaffiche dans la liste le choix sélectionné et stocké dans chrome.storage
   const restoreOptions = () => {
+    chrome.storage.sync.get(
+      {choixAudio: true},
+      (items) => {
+        document.getElementById('toggleAudio').checked = items.choixAudio;
+      }
+    );
     chrome.storage.sync.get(
       //.get : récupère l'info dans chrome.storage
       { langueDeTraduction: 'FR' },
@@ -36,4 +51,4 @@ const saveOptions = () => {
   //Analyse le DOM de la page html et appelle restoreOptions
   document.getElementById('save').addEventListener('click', saveOptions);
   //Appelle saveOptions lors du 'click' sur le bouton 'save'
-  
+  document.getElementById('toggleAudio').addEventListener('change', saveAudio);
--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@
 //L'API envoie le code à 2 lettres, on passe par une fonction pour afficher la langue en entier
 
 const tableauLangue = {
-<<<<<<< HEAD
   BG: "Bulgare",
   DA: "Danois",
   CS: "Tchèque",
@@ -36,54 +35,15 @@ const tableauLangue = {
 
 function changeLangue(lettre) {
   let language = tableauLangue[lettre];
-  console.log(language);
-  return language;
-=======
-  "BG" : "Bulgare",
-  "DA" : "Danois",
-  "CS" : "Tchèque",
-  "DE" : "Allemand",
-  "EL" : "Grec",
-  "EN" : "Anglais",
-  "ES" : "Espagnol",
-  "ET" : "Estonien",
-  "FI" : "Finnois",
-  "FR" : "Français",
-  "HU" : "Hongrois",
-  "ID" : "Indonesien",
-  "IT" : "Italien",
-  "JA" : "Japonais",
-  "KO" : "Coréen",
-  "LT" : "Lituanien",
-  "LV" : "Letton",
-  "NB" : "Norvégien",
-  "NL" : "Néerlandais",
-  "PL" : "Polonais",
-  "PT" : "Portugais",
-  "RO" : "Roumain",
-  "RU" : "Russe",
-  "SK" : "Slovaque",
-  "SL" : "Slovène",
-  "SV" : "Suèdois",
-  "TR" : "Turc",
-  "UK" : "Ukrainien",
-  "ZH" : "Chinois"
-}
-
-function changeLangue(lettre) {
-  let language = tableauLangue[lettre]
   //lettre : colonne de gauche, info de l'API
   //language : colonne de droite
-  console.log(language)
-  return language
->>>>>>> 27ccb5f (enregistrement de la langue dans les options, OK. pas encore retransmis pour la traduction. +commentaires partout.)
+  console.log(language);
+  return language;
 }
-
 
 //Appel à l'API
 
 (async () => {
-<<<<<<< HEAD
   const [tab] = await chrome.tabs.query({
     active: true,
     lastFocusedWindow: true,
@@ -111,37 +71,6 @@ function changeLangue(lettre) {
       }),
     }
   );
-=======
-  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-  //Verifier l'onglet actif de chrome
-  
-  const response = await chrome.tabs.sendMessage(tab.id, { action: "declenche" });
-  //communication popup -> content-script pour demander l'info du mot sélectionné
-  //message précis
-  console.log(response);
-
-  chrome.storage.sync.get({langueDeTraduction: 'FR'}, async (items) => {
-    const langueCible = items.langueDeTraduction;
-  //Récupération de la valeur associée à la clé langueDeTraduction, stockée dans chrome.storage
-  //grâce à la fonction restoreOptions dans options.js
-  //Valeur par défaut : FR (français) 
-  console.log(langueCible);
-  
-  
-  //API
-const translationResponse = await fetch('https://api-free.deepl.com/v2/translate', {
-    method: 'POST',
-    headers: {
-      'Authorization': 'DeepL-Auth-Key b36b1d4d-3421-06bc-ac98-82b672da6d3c:fx',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      text: [response],
-      //response = envoyée par content-script
-      target_lang: langueCible
-    })
-  });
->>>>>>> 27ccb5f (enregistrement de la langue dans les options, OK. pas encore retransmis pour la traduction. +commentaires partout.)
 
   const translationData = await translationResponse.json();
   //traduction en format json, non exploitable en l'état
@@ -153,27 +82,16 @@ const translationResponse = await fetch('https://api-free.deepl.com/v2/translate
   console.log(codeLanguage);
   console.log(translationData);
 
-<<<<<<< HEAD
   let language = changeLangue(codeLanguage);
+  //Transformation de la langue de la sélection
   document.getElementById("lang").innerHTML = `Langage : ${language}`;
+  //Affichage de la langue de la sélection
   document.getElementById(
     "trad"
   ).innerHTML = `Traduction : ${translatedResponse}`;
-})();
-
-let son = new Audio("cliquetis.flac");
-son.play();
-=======
-  let language = changeLangue(codeLanguage)
-  //Transformation de la langue de la sélection
-  document.getElementById('lang').innerHTML = `Langage : ${language}`;
-  //Affichage de la langue de la sélection
-  document.getElementById('trad').innerHTML = `Traduction : ${translatedResponse}`;
   //Affichage de la traduction
-})
 })();
 
 //Son de l'extension, joué au clic
-let son = new Audio("cliquetis.flac")
-son.play()
->>>>>>> 27ccb5f (enregistrement de la langue dans les options, OK. pas encore retransmis pour la traduction. +commentaires partout.)
+let son = new Audio("cliquetis.flac");
+son.play();
